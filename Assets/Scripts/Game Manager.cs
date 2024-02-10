7ba55e5afccba8a77ef890f05ed445a407c861ab
Alexandre Lino <149592058+alexlinofer@users.noreply.github.com>
2024-02-10 16:24:21 -0300

    Final Adjustments Game Manager Script and Scenes Menu and Level 01

--- Assets/Scripts/Game Manager.cs
+++ Assets/Scripts/Game Manager.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using Plataforma2D.Singleton;
 using DG.Tweening;
+using System.Threading;
+using Unity.VisualScripting;
 
-public class GameManager : Singleton<GameManager>
+public class GameManager : Plataforma2D.Singleton.Singleton<GameManager>
 {
     [Header("Player")]
     public GameObject playerPrefab;
@@ -16,6 +18,7 @@ public class GameManager : Singleton<GameManager>
     public Transform startPoint;
 
     private GameObject _currentPlayer;
+    private Transform _playerPrefab;
 
     [Header("Animation")]
     public float duration = .5f;
@@ -37,6 +40,20 @@ public class GameManager : Singleton<GameManager>
         _currentPlayer = Instantiate(playerPrefab);
         _currentPlayer.transform.position = startPoint.transform.position;
         _currentPlayer.transform.DOScale(0, duration).SetEase(ease).From().SetDelay(delay);
+        _currentPlayer.AddComponent<PlayerController>();
+
+    }
+
+    public void GrowPlayerMethod()
+    {
+        _currentPlayer.GetComponent<PlayerController>().StartCoroutine(GrowPlayer());
+    }
+
+    IEnumerator GrowPlayer()
+    {
+        _currentPlayer.transform.DOScale(2, duration).SetEase(ease);
+        yield return new WaitForSeconds(3);
+        _currentPlayer.transform.DOScale(1, duration).SetEase(ease);
     }
 
 }
