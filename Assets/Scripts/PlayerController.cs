46c9162f427e4fd94227f6fa0314585bde83bcca
Alexandre Lino <149592058+alexlinofer@users.noreply.github.com>
2024-02-10 16:23:43 -0300

    Add PlayerController Component
    Creation of PlayerController Script just so the player can grow when prompted

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
