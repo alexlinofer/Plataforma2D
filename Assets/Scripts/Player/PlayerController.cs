58f73d81a4764b4ccb9b45dd6ed7b28f25f9a981
Alexandre Lino <149592058+alexlinofer@users.noreply.github.com>
2024-02-14 15:29:18 -0300

    Added Jump
    Edited the PlayerController script to add jump mechanics and implemented a IEnumerator to stop it from jumping anytime

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -10,8 +10,13 @@ public class PlayerController : MonoBehaviour
     public Vector2 friction = new Vector2(1f, 0);
 
     public float speed;
+    public float speedRun;
+
+    private float _currentSpeed;
 
     public float forceJump = 10;
+
+    private bool _isJumping = false;
 
     private void Update()
     {
@@ -21,15 +26,21 @@ public class PlayerController : MonoBehaviour
 
     private void HandleMovement()
     {
+        if (Input.GetKey(KeyCode.LeftControl))
+            _currentSpeed = speedRun;
+        else
+            _currentSpeed = speed;
+       
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myrb.MovePosition(myrb.position - velocity * Time.deltaTime);
-            myrb.velocity = new Vector2(-speed, myrb.velocity.y);
+            myrb.velocity = new Vector2(-_currentSpeed, myrb.velocity.y);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             //myrb.MovePosition(myrb.position + velocity * Time.deltaTime);
-            myrb.velocity = new Vector2(speed, myrb.velocity.y);
+            myrb.velocity = new Vector2(_currentSpeed, myrb.velocity.y);
 
         }
 
@@ -47,10 +58,18 @@ public class PlayerController : MonoBehaviour
 
     private void HandleJump()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && !_isJumping)
         {
-            myrb.velocity = Vector2.up * forceJump;
-
+            StartCoroutine(JumpTimer());
         }
+
+    }
+
+    IEnumerator JumpTimer()
+    {
+         _isJumping = true;
+         myrb.velocity = Vector2.up * forceJump;
+         yield return new WaitForSeconds(1);
+         _isJumping = false;
     }
 }
