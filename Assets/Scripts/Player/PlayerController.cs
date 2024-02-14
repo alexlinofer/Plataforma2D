b2647681e062dc13ae9ac577bd14e8a3f372c7c0
Alexandre Lino <149592058+alexlinofer@users.noreply.github.com>
2024-02-14 16:28:28 -0300

    Add Jump Animation
    Added Jump Animation to the PlayerController script, using the DOTween pack

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,21 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using DG.Tweening;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D myrb;
 
-    public Vector2 friction = new Vector2(1f, 0);
 
+    [Header("Speed Setup")]
+    public Vector2 friction = new Vector2(1f, 0);
     public float speed;
     public float speedRun;
-
-    private float _currentSpeed;
-
     public float forceJump = 10;
 
     private bool _isJumping = false;
+    private float _currentSpeed;
+
+
+    [Header("Animation Setup")]
+    public float jumpScaleY = 1.5f;
+    public float jumpScaleX = 0.7f;
+    /*public float squishyScaleX = 1.5f;
+    public float squishyScaleY = 0.7f;
+    public float squishyanimationDuration = 0.3f;*/
+    public float animationDuration = .3f;
+    public Ease ease = Ease.OutBack;
+    //public GameObject player;
+    
+
 
     private void Update()
     {
@@ -61,15 +75,43 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow) && !_isJumping)
         {
             StartCoroutine(JumpTimer());
+            HandleScaleJump();
         }
-
     }
 
     IEnumerator JumpTimer()
     {
-         _isJumping = true;
-         myrb.velocity = Vector2.up * forceJump;
-         yield return new WaitForSeconds(1);
+        _isJumping = true;
+        myrb.velocity = Vector2.up * forceJump;
+        myrb.transform.localScale = Vector2.one;
+        DOTween.Kill(myrb.transform);
+
+
+         yield return new WaitForSeconds(0.9f);
          _isJumping = false;
     }
+
+    /*private void OnCollisionEnter2D(Collision2D collision)
+    {
+      if (gameObject.CompareTag("Floor") && !_isJumping)
+        {
+            myrb.transform.DOScaleX(squishyScaleX, squishyanimationDuration).SetLoops(2, LoopType.Yoyo);
+            myrb.transform.DOScaleY(squishyScaleY, squishyanimationDuration).SetLoops(2, LoopType.Yoyo);
+        } 
+    }*/
+
+    private void HandleScaleJump()
+    {
+        myrb.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        myrb.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+    }
+
+    /*IEnumerator AnimationScaleJump()
+    {
+        myrb.transform.DOScaleX(squishyScaleX, squishyanimationDuration).SetLoops(2, LoopType.Yoyo);
+        myrb.transform.DOScaleY(squishyScaleY, squishyanimationDuration).SetLoops(2, LoopType.Yoyo);
+        yield return new WaitForSeconds(squishyanimationDuration);
+        myrb.transform.DOScaleY(jumpScaleY, animationDuration).SetLoops(2, LoopType.Yoyo);
+        myrb.transform.DOScaleX(jumpScaleX, animationDuration).SetLoops(2, LoopType.Yoyo);
+    }*/
 }
