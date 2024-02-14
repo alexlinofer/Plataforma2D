0312bbc577dc8f23a557fcdfaf7166d57dc1ee77
Alexandre Lino <149592058+alexlinofer@users.noreply.github.com>
2024-02-14 11:20:27 -0300

    Add Jump Control
    Edited PlayerController Script, movement Method is now inside HandleMovement(). Created HandleJump. Deleted the "velocity" variable. Created the friction variable

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,25 +7,50 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D myrb;
 
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(1f, 0);
 
     public float speed;
 
+    public float forceJump = 10;
+
     private void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        HandleJump();
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myrb.MovePosition(myrb.position - velocity * Time.deltaTime);
-            myrb.velocity = new Vector2(-speed, velocity.y);
-
+            myrb.velocity = new Vector2(-speed, myrb.velocity.y);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             //myrb.MovePosition(myrb.position + velocity * Time.deltaTime);
-            myrb.velocity = new Vector2(speed, velocity.y);
+            myrb.velocity = new Vector2(speed, myrb.velocity.y);
 
         }
+
+        if(myrb.velocity.x > 0)
+        {
+            myrb.velocity -= friction;
+        }
+        else if(myrb.velocity.x < 0)
+        {
+            myrb.velocity += friction;
+        }
+
     }
 
 
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            myrb.velocity = Vector2.up * forceJump;
+
+        }
+    }
 }
